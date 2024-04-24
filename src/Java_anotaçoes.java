413aa4151646337bc06bec16f23fa45a3646c377
will <willian.antonio.silva.1998@gmail.com>
2024-04-24 18:05:45 -0300

    Primeiro commit

---
+++ src/Java_anotaçoes.java
@@ -0,0 +1,17 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class Java_anotaçoes {
+
+
+    public static void main(String[] args) {
+        String nome = "Julia";
+        double meta = 1000;
+        double vendas = 5000;
+
+        if (vendas > meta)
+            System.out.println(nome  +  "Não bateu a meta");
+
+    }
+
+
+}
---
+++ src/tste.java
@@ -0,0 +1,8 @@
+public class tste {
+    // Hello.java
+        public static void main(String[] args) {
+            System.out.println("Olá, Jenkins! Este é um exemplo de automação em Java.");
+        }
+    }
+
+
